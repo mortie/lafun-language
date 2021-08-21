a77f450742f33f2b83ddcb81c6f7080bd22dfef0
Martin Dørum <martid0311@gmail.com>
2021-08-21 15:41:01 +0200

    small example main() which exercises the lexer

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,33 @@
 #include "parse.h"
+#include "Lexer.h"
 
-int main() {
+#include <fstream>
+#include <iostream>
+
+using namespace lafun;
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		return 1;
+	}
+
+	std::ifstream stream(argv[1]);
+	std::string str;
+	while (stream.good() && !stream.eof()) {
+		char buf[1024];
+		std::streamsize n = stream.readsome(buf, sizeof(buf));
+		if (n == 0) break;
+		str.append(buf, n);
+	}
+
+	Lexer lexer(str);
+	while (true) {
+		Token tok = lexer.readTok();
+		std::cout << tok.toString() << '\n';
+		if (tok.tok == Tok::E_O_F) {
+			break;
+		}
+	}
+
+	return 0;
 }
