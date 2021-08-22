a46544159ef27016422dabb9ffeab20054fd878a
Martin Dørum <martid0311@gmail.com>
2021-08-22 16:29:14 +0200

    prelude stuff

--- src/fun/prelude.cc
+++ src/fun/prelude.cc
@@ -2,23 +2,41 @@
 
 namespace fun {
 
-std::string prelude = R"javascript(// <Prelude>
+std::string prelude = R"javascript(/* <Prelude> */
+class FUNclass_Array {
+	constructor() {
+		this.data = [];
+	}
+
+	push(val) {
+		return this.data.push(val);
+	}
+
+	pop() {
+		return this.data.pop()
+	}
+
+	get(idx) {
+		return this.data[idx];
+	}
+
+	set(idx, val) {
+		return this.data[idx] = val;
+	}
+}
+
 function FUN_Array() {
-	return new Array();
+	return new FUNclass_Array();
 }
 
-function FUN_Map() {
-	return new Map();
-}
-
-function FUN__print() {
+function FUN_print() {
 	console.log.apply(console, arguments);
 }
-// </Prelude>
+/* </Prelude> */
 )javascript";
 
 const std::vector<std::string> preludeNames = {
-	"Array", "Map", "print",
+	"Array", "print",
 };
 
 }
