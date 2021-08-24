156620750ca92316836e4bcd35a1cc1901ac3eb8
Martin Dørum <martid0311@gmail.com>
2021-08-24 16:57:19 +0200

    some prelude changes to expose a typeof function and a map type

--- src/fun/prelude.cc
+++ src/fun/prelude.cc
@@ -23,14 +23,65 @@ class FUNclass_Array {
 	set(idx, val) {
 		return this.data[idx] = val;
 	}
+
+	get length() {
+		return this.data.length;
+	}
 }
 
 function FUN_Array() {
 	return new FUNclass_Array();
 }
 
+class FUNclass_Map {
+	constructor() {
+		this.data = new Map();
+	}
+
+	set(key, val) {
+		this.data.set(key, val);
+	}
+
+	get(key) {
+		return this.data.get(key);
+	}
+
+	keys() {
+		let arr = FUN_Array();
+		for (let key of this.data.keys()) {
+			arr.push(key);
+		}
+		return arr;
+	}
+}
+
+function FUN_Map() {
+	return new FUNclass_Map();
+}
+
 function FUN_print() {
 	console.log.apply(console, arguments);
+}
+
+function FUN_typeof(val) {
+	let t = typeof val;
+	if (t == "number" || t == "string" || t == "boolean") {
+		return t;
+	}
+
+	if (t == null) {
+		return "none";
+	}
+
+	if (t == "object") {
+		if (val instanceof FUNclass_Array) {
+			return "array";
+		} else if (val instanceof FUNclass_Map) {
+			return "map";
+		}
+	}
+
+	return "jsval";
 }
 
 let FUN_math = Math;
@@ -45,7 +96,7 @@ FUN_main();
 )javascript";
 
 const std::vector<std::string> preludeNames = {
-	"Array", "print", "math", "true", "false", "none",
+	"Array", "Map", "print", "typeof", "math", "true", "false", "none",
 };
 
 }
